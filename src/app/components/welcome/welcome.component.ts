d85bb099f6106a56a50b64bf09e8a57e3d04a0c6
Emma Paris <90420888+ramiberlat@users.noreply.github.com>
2023-02-04 14:09:07 -0500

    ;

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
+
   templateUrl: './welcome.component.html',
+
   styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent {}
