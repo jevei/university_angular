d0be334427e204376e4648d5c1e46fbd0598030a
jevei <j.veillette@hotmail.ca>
2023-02-04 08:51:35 -0500

    component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    WelcomeComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
