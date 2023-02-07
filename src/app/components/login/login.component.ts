c1d12197c2eaeceb629eb667f4cf84b7631df2cb
jevei <j.veillette@hotmail.ca>
2023-02-07 15:43:02 -0500

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +11,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor() {
+  constructor(private authService: AuthService, private router: Router) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [
@@ -20,5 +22,20 @@ export class LoginComponent implements OnInit {
   }
   ngOnInit(): void {}
 
-  logIn() {}
+  logIn() {
+    this.authService
+      .userLogin(
+        this.loginForm.get('email')?.value,
+        this.loginForm.get('password')?.value
+      )
+      .subscribe((success) => {
+        if (success) {
+          this.router.navigate(['/']);
+          console.log('la team de JXR est trop forte');
+        } else {
+          console.log('ERROR');
+          alert('Email ou Mot de Passe invalide.');
+        }
+      });
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +10,7 @@ import { catchError, tap } from 'rxjs/operators';
 export class AuthService {
   private _currentUser: User | null = null;
   private usersUrl = 'api/users';
+  private readonly CURRENT_USER_KEY = 'jxr.users.currentUser';
 
   get currentUser(): User | null {
     return this._currentUser;
@@ -25,7 +26,7 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
-  getUsers(): Observable<User[]> {
+  /*getUsers(): Observable<User[]> {
     return this.http
       .get<User[]>(this.usersUrl)
       .pipe(catchError(this.handleError<User[]>('getUsers', [])));
@@ -36,7 +37,7 @@ export class AuthService {
     return this.http
       .get<User>(url)
       .pipe(catchError(this.handleError<User>(`getUser id=${id}`)));
-  }
+  }*/
 
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
@@ -46,11 +47,61 @@ export class AuthService {
     };
   }
 
+  private setCurrentUser(user: User | null) {
+    this._currentUser = user;
+    sessionStorage.setItem(this.CURRENT_USER_KEY, JSON.stringify(user));
+    console.log(this._currentUser);
+  }
+
   userRegistration(newUser: User): Observable<User> {
     return this.http
-      .post<any>(this.usersUrl, newUser, this.httpOptions)
+      .post<User>(this.usersUrl, newUser, this.httpOptions)
       .pipe(catchError(this.handleError<User>('adduser')));
   }
+
+  userLogin(email: string, password: string): Observable<any> {
+    /*var users = this.getUsers();
+    console.log(users);
+    return this.login(email, password);*/
+    return this.http.get<User[]>(this.usersUrl).pipe(
+      map((response) => {
+        var retour: boolean = false;
+        response.forEach((_user) => {
+          if (_user.email == email && _user.password == password) {
+            this.setCurrentUser(_user);
+            retour = true;
+          }
+        });
+        return retour;
+        //this.login(id);
+      }),
+      catchError(this.handleError<User[]>('getUsers', []))
+    );
+  }
+
+  /*login(id: number): Observable<any> {
+    const url = `${this.usersUrl}/${id}`;
+    return this.http.get<User>(url).pipe(
+      map((response) => {
+        if (response) {
+          //le .success, pour être vrait doit être fournie par l'api, donc pour l'instant il est toujours faux
+          console.log('New User service : ', response);
+
+          var data = response;
+          console.log('New User service : ', data);
+          let newUser: User = new User();
+          newUser = data;
+          console.log('New user value ', newUser);
+
+          //this.setCurrentUser(newUser);
+          return true;
+        } else {
+          return false;
+        }
+      }),
+      catchError(this.handleError<User>(`getUser id=${id}`))
+    );
+  }*/
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
