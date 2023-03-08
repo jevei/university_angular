1fe51c37e12b7f428bdb97d1c95f41f66535f588
jevei <j.veillette@hotmail.ca>
2023-03-08 16:38:45 -0500

    secureRequest exemple

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private auth: AuthService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const secureReq = req.clone({
+      url: req.url.replace('http://', 'https://'),
+    });
+    return next.handle(secureReq);
+  }
+}
