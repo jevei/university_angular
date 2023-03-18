6d8a538bfca710a49493b3ce992c35e423c3bb1e
jevei <j.veillette@hotmail.ca>
2023-03-18 10:59:52 -0400

    some guard

---
+++ src/app/guards/login-access.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginAccessGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate(
+    _route: ActivatedRouteSnapshot,
+    _state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    console.log('guard product', this.authService.isLoggedIn);
+    if (this.authService.isLoggedIn) {
+      return true;
+    } else {
+      // sinon, on peut fournir un URLTree pour rediriger l'utilisateur
+      return this.router.parseUrl('/products');
+    }
+  }
+}
